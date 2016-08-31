824e50157bdb4b1cc8c6db1b24f5ad9b713a3dbc
James Lucas <rum_drums@yahoo.com>
2016-08-30 23:23:49 -0500

    working again with new structure

--- kiva.go
+++ kiva.go
@@ -1,106 +1,20 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"github.com/rumdrums/go-kiva/kiva"
 )
 
-type Loan struct {
-	Name                   string              `json:"name"`
-	Location               LocationData        `json: "Location"`
-	PostedDate             int                 `json: "posted_date"`
-	Activity               string              `json: "activity"`
-	Id                     int                 `json: "id"`
-	Use                    string              `json: "use"`
-	Desc                   Description         `json: "description"`
-	FundedAmount           int                 `json: "funded_amount"`
-	PartnerId              int                 `json: "partner_id"`
-	Image                  Image               `json: "image"`
-	BorrowerCount          int                 `json: "borrower_count"`
-	LoanAmount             int                 `json: "loan_amount"`
-	Status                 string              `json: "status"`
-	Sector                 string              `json: "sector"`
-	Expiration             int                 `json: "planned_expiration_date"`
-	BonusCreditEligibility bool                `json: "bonus_credit_eligibility"`
-	Tags                   []map[string]string `json: "tags"`
-	BasketAmount           int                 `json: "basket_amount"`
-}
-
-type Image struct {
-	TemplateId int `json: "template_id"`
-	Id         int `json: "id"`
-}
-
-type Description struct {
-	Languages []string `json: "languages"`
-}
-
-type LocationData struct {
-	Country string            `json: "country"`
-	Geo     map[string]string `json: "geo"`
-	Town    string            `json: "town"`
-}
-
-type PagingData struct {
-	Total    int `json: "total"`
-	Page     int `json: "page"`
-	PageSize int `json: "page_size"`
-	Pages    int `json: "pages"`
-}
-
-type PagedLoansResponse struct {
-	Paging PagingData `json: "paging"`
-	Loans  []Loan     `json: "loans"`
-}
-
-type UnpagedLoansResponse struct {
-	Loans []Loan `json: "loans"`
-}
-
-func Client() *client.Client {
-	return client.New(&clientConfig)
-}
-
-func GetResponse(url string) (*http.Response, error) {
-	urlBase := "http://api.kivaws.org/v1"
-	r, err := http.Get(urlBase + url + ".json")
-	if err != nil {
-		return r, err
-	}
-	return r, nil
-}
-
-
-
-func PrintRawLoansJson() {
-
-	var dat map[string]interface{}
-
-	url := "/loans/newest"
-	resp, err := GetResponse(url)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	bs, _ := ioutil.ReadAll(resp.Body)
-	err = json.Unmarshal(bs, &dat)
-	if err != nil {
-		fmt.Println(err)
-	}
-	for k, v := range dat {
-		fmt.Println(k)
-		fmt.Println(v)
-	}
+func Client() *kiva.Client {
+	var clientConfig kiva.Config
+	return kiva.New(&clientConfig)
 }
 
 func main() {
 	//PrintRawLoansJson()
 
  	cli := Client()
-	loans, err := GetLoansById(1132720, 1128815)
+	loans, err :=  cli.GetLoansById(1137156, 1128815)
 	if err != nil {
 		fmt.Println(err)
 	}
--- kiva/client.go
+++ kiva/client.go
@@ -1,13 +1,69 @@
 package kiva
 
 import (
+    "io"
     "net/http"
     "net/url"
     "fmt"
     "encoding/json"
-    "path"
     "time"
+    "path"
+    "errors"
+    "strconv"
+
 )
+
+type Loan struct {
+  Name                   string              `json:"name"`
+  Location               LocationData        `json: "Location"`
+  PostedDate             int                 `json: "posted_date"`
+  Activity               string              `json: "activity"`
+  Id                     int                 `json: "id"`
+  Use                    string              `json: "use"`
+  Desc                   Description         `json: "description"`
+  FundedAmount           int                 `json: "funded_amount"`
+  PartnerId              int                 `json: "partner_id"`
+  Image                  Image               `json: "image"`
+  BorrowerCount          int                 `json: "borrower_count"`
+  LoanAmount             int                 `json: "loan_amount"`
+  Status                 string              `json: "status"`
+  Sector                 string              `json: "sector"`
+  Expiration             int                 `json: "planned_expiration_date"`
+  BonusCreditEligibility bool                `json: "bonus_credit_eligibility"`
+  Tags                   []map[string]string `json: "tags"`
+  BasketAmount           int                 `json: "basket_amount"`
+}
+
+type Image struct {
+  TemplateId int `json: "template_id"`
+  Id         int `json: "id"`
+}
+
+type Description struct {
+  Languages []string `json: "languages"`
+}
+
+type LocationData struct {
+  Country string            `json: "country"`
+  Geo     map[string]string `json: "geo"`
+  Town    string            `json: "town"`
+}
+
+type PagingData struct {
+  Total    int `json: "total"`
+  Page     int `json: "page"`
+  PageSize int `json: "page_size"`
+  Pages    int `json: "pages"`
+}
+
+type PagedLoansResponse struct {
+  Paging PagingData `json: "paging"`
+  Loans  []Loan     `json: "loans"`
+}
+
+type UnpagedLoansResponse struct {
+  Loans []Loan `json: "loans"`
+}
 
 type doer interface {
   Do(*http.Request) (*http.Response, error)
@@ -46,9 +102,12 @@ func New(config *Config) *Client {
 }
 
 // make the actual http request and return the http response
-func (c *Client) raw(method string, path string, query url.Values, body io.Reader) (*http.Response, error) {
-  url := c.BaseURL
-  url.Path = path.Join(c.baseURL,path)
+func (c *Client) raw(method string, urlpath string, query url.Values, body io.Reader) (*http.Response, error) {
+  url := c.baseURL
+  // FIXME: ".json" should probably be an option:
+  urlpath += ".json"
+  url.Path = path.Join(c.baseURL.Path, urlpath)
+  fmt.Println(url.String())
   url.RawQuery = query.Encode()
   req, err := http.NewRequest(method, url.String(), body)
   if err != nil {
@@ -59,8 +118,8 @@ func (c *Client) raw(method string, path string, query url.Values, body io.Reade
 
 // decode json from http response and return as interface understood
 // by caller
-func (c *Client) do(method string, path string, query url.Values, body io.Reader, v interface{}) error {
-  resp, err := c.raw(method, path, query, body)
+func (c *Client) do(method string, urlpath string, query url.Values, body io.Reader, v interface{}) error {
+  resp, err := c.raw(method, urlpath, query, body)
   if err != nil {
     return fmt.Errorf("error making request: %s", err)
   }
@@ -74,7 +133,7 @@ func (c *Client) do(method string, path string, query url.Values, body io.Reader
 func (c *Client) GetLoansById(ids ...int) ([]Loan, error) {
   // not sure whether requesting 50 loan IDs will return paged results
 
-  var baseUrl = "/loans/"
+  var baseUrl = "/v1/loans/"
   var url string
   var err error
   var loans []Loan
@@ -92,11 +151,11 @@ func (c *Client) GetLoansById(ids ...int) ([]Loan, error) {
   }
   
   var lr UnpagedLoansResponse
-  r, err := c.do("GET", baseUrl + url, nil, nil, &lr)
+  err = c.do("GET", baseUrl + url, nil, nil, &lr)
   if err != nil {
     return nil, err
   }
-  
+
   return lr.Loans, nil
 }
 
