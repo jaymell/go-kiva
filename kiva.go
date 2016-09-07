2c68e4bf145f28b98f81624b64a1fba10083ecab
James Lucas <jamesmlucas@gmail.com>
2016-09-07 02:20:17 -0500

    getloanteams in progress

--- kiva.go
+++ kiva.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"os"
 	"fmt"
 	"github.com/rumdrums/go-kiva/kiva"
+	"os"
 	"strconv"
 )
 
@@ -15,41 +15,40 @@ func Client() *kiva.Client {
 func main() {
 	//PrintRawLoansJson()
 
- 	cli := Client()
- 	var loanIDs []int
-    for _, v := range os.Args[1:] {
-    	if id, e := strconv.Atoi(v); e == nil {
-      	  loanIDs = append(loanIDs, id)
-    	}
-    	//loanIDs[i], _ = strconv.Atoi(v)
-    }
-    loans, err := cli.GetLoansByID(loanIDs...)
-    if err != nil {
+	cli := Client()
+	var loanIDs []int
+	for _, v := range os.Args[1:] {
+		if id, e := strconv.Atoi(v); e == nil {
+			loanIDs = append(loanIDs, id)
+		}
+		//loanIDs[i], _ = strconv.Atoi(v)
+	}
+	loans, err := cli.GetLoansByID(loanIDs...)
+	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println("loans ", loans)
 
 	for k, v := range loans {
 		fmt.Println(k, v)
-	}	
+	}
 
-    for _, v := range loanIDs {
-      lenders, err := cli.GetLoanLenders(v)
-      if err != nil {
-      	fmt.Println(err)
-      	break
-      } 
-      for _, w := range lenders {
-      	fmt.Println("lender: ", w)
-      }
-      similar, err := cli.GetSimilarLoans(v)
-      if err != nil {
-        fmt.Println(err)
-        break
-      } 
-      for _, w := range similar {
-        fmt.Println("similar: ", w)
-      }
-    }
+	for _, v := range loanIDs {
+		lenders, err := cli.GetLoanLenders(v)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		for _, w := range lenders {
+			fmt.Println("lender: ", w)
+		}
+		similar, err := cli.GetSimilarLoans(v)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		for _, w := range similar {
+			fmt.Println("similar: ", w)
+		}
+	}
 }
-
--- kiva/client.go
+++ kiva/client.go
@@ -1,198 +1,200 @@
 package kiva
 
 import (
-    "io"
-    "net/http"
-    "net/url"
-    "fmt"
-    "encoding/json"
-    "time"
-    "path"
-    "errors"
-    "strconv"
-
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"path"
+	"strconv"
+	"time"
 )
 
 type Loan struct {
-  Name                   string              `json:"name"`
-  Location               LocationData        `json: "Location"`
-  PostedDate             int                 `json: "posted_date"`
-  Activity               string              `json: "activity"`
-  ID                     int                 `json: "id"`
-  Use                    string              `json: "use"`
-  Desc                   Description         `json: "description"`
-  FundedAmount           int                 `json: "funded_amount"`
-  PartnerID              int                 `json: "partner_id"`
-  Image                  Image               `json: "image"`
-  BorrowerCount          int                 `json: "borrower_count"`
-  LoanAmount             int                 `json: "loan_amount"`
-  Status                 string              `json: "status"`
-  Sector                 string              `json: "sector"`
-  Expiration             int                 `json: "planned_expiration_date"`
-  BonusCreditEligibility bool                `json: "bonus_credit_eligibility"`
-  Tags                   []map[string]string `json: "tags"`
-  BasketAmount           int                 `json: "basket_amount"`
+	Name                   string              `json:"name"`
+	Location               LocationData        `json: "Location"`
+	PostedDate             int                 `json: "posted_date"`
+	Activity               string              `json: "activity"`
+	ID                     int                 `json: "id"`
+	Use                    string              `json: "use"`
+	Desc                   Description         `json: "description"`
+	FundedAmount           int                 `json: "funded_amount"`
+	PartnerID              int                 `json: "partner_id"`
+	Image                  Image               `json: "image"`
+	BorrowerCount          int                 `json: "borrower_count"`
+	LoanAmount             int                 `json: "loan_amount"`
+	Status                 string              `json: "status"`
+	Sector                 string              `json: "sector"`
+	Expiration             int                 `json: "planned_expiration_date"`
+	BonusCreditEligibility bool                `json: "bonus_credit_eligibility"`
+	Tags                   []map[string]string `json: "tags"`
+	BasketAmount           int                 `json: "basket_amount"`
+}
+
+type Team struct {
+	Category string `json: "cateogry"`
 }
 
 type Lender struct {
-  ID int `json: "lender_id"`
-  Name string `json: "name"`
-  Whereabouts string `json: "whereabots"`
-  CountryCode string `json: "country_code"`
-  Image
+	ID          int    `json: "lender_id"`
+	Name        string `json: "name"`
+	Whereabouts string `json: "whereabots"`
+	CountryCode string `json: "country_code"`
+	Image
 }
 
 type Repayment struct {
- // can't find data on this 
+	// can't find data on this
 }
 
 type Image struct {
-  TemplateID int `json: "template_id"`
-  ID         int `json: "id"`
+	TemplateID int `json: "template_id"`
+	ID         int `json: "id"`
 }
 
 type Description struct {
-  Languages []string `json: "languages"`
+	Languages []string `json: "languages"`
 }
 
 type LocationData struct {
-  Country string            `json: "country"`
-  Geo     map[string]string `json: "geo"`
-  Town    string            `json: "town"`
+	Country string            `json: "country"`
+	Geo     map[string]string `json: "geo"`
+	Town    string            `json: "town"`
 }
 
 type PagingData struct {
-  Total    int `json: "total"`
-  Page     int `json: "page"`
-  PageSize int `json: "page_size"`
-  Pages    int `json: "pages"`
+	Total    int `json: "total"`
+	Page     int `json: "page"`
+	PageSize int `json: "page_size"`
+	Pages    int `json: "pages"`
 }
 
-
 type doer interface {
-  Do(*http.Request) (*http.Response, error)
+	Do(*http.Request) (*http.Response, error)
 }
 
 type Client struct {
-  baseURL url.URL
-  doer doer
+	baseURL url.URL
+	doer    doer
 }
 
 type Config struct {
-  BaseURL string
+	BaseURL string
 }
 
 func New(config *Config) *Client {
-  if config == nil || config.BaseURL == "" {
-    return &Client{
-        baseURL: url.URL{
-          Scheme: "http",
-          Host: "api.kivaws.org",
-        },
-      doer: &http.Client{
-        Timeout: time.Second * 10,
-      },
-    }
-  }
+	if config == nil || config.BaseURL == "" {
+		return &Client{
+			baseURL: url.URL{
+				Scheme: "http",
+				Host:   "api.kivaws.org",
+			},
+			doer: &http.Client{
+				Timeout: time.Second * 10,
+			},
+		}
+	}
 
-  baseURL, err := url.Parse(config.BaseURL)
-  if err != nil {
-    panic(fmt.Sprintf("cannot parse base URL: %q (%v)", config.BaseURL, err))
-  }
-  return &Client{
-    baseURL: *baseURL,
-    doer: &http.Client{},
-  }
+	baseURL, err := url.Parse(config.BaseURL)
+	if err != nil {
+		panic(fmt.Sprintf("cannot parse base URL: %q (%v)", config.BaseURL, err))
+	}
+	return &Client{
+		baseURL: *baseURL,
+		doer:    &http.Client{},
+	}
 }
 
 // make the actual http request and return the http response
 func (c *Client) raw(method string, urlpath string, query url.Values, body io.Reader) (*http.Response, error) {
-  url := c.baseURL
-  // FIXME: ".json" should probably be an option:
-  urlpath += ".json"
-  url.Path = path.Join(c.baseURL.Path, urlpath)
-  fmt.Println(url.String())
-  url.RawQuery = query.Encode()
-  req, err := http.NewRequest(method, url.String(), body)
-  if err != nil {
-    return nil, err
-  }
-  return c.doer.Do(req)
+	url := c.baseURL
+	// FIXME: ".json" should probably be an option:
+	urlpath += ".json"
+	url.Path = path.Join(c.baseURL.Path, urlpath)
+	fmt.Println(url.String())
+	url.RawQuery = query.Encode()
+	req, err := http.NewRequest(method, url.String(), body)
+	if err != nil {
+		return nil, err
+	}
+	return c.doer.Do(req)
 }
 
 // decode json from http response and return as interface understood
 // by caller
 func (c *Client) do(method string, urlpath string, query url.Values, body io.Reader, v interface{}) error {
-  resp, err := c.raw(method, urlpath, query, body)
-  if err != nil {
-    return fmt.Errorf("error making request: %s", err)
-  }
-  decode := json.NewDecoder(resp.Body)
-  if err = decode.Decode(&v); err !=nil {
-    return fmt.Errorf("cannot decode your dumb response: %s", err)
-  }
-  return nil
+	resp, err := c.raw(method, urlpath, query, body)
+	if err != nil {
+		return fmt.Errorf("error making request: %s", err)
+	}
+	decode := json.NewDecoder(resp.Body)
+	if err = decode.Decode(&v); err != nil {
+		return fmt.Errorf("cannot decode your dumb response: %s", err)
+	}
+	return nil
 }
 
 type PagedLoansResponse struct {
-  Paging PagingData `json: "paging"`
-  Loans  []Loan     `json: "loans"`
+	Paging PagingData `json: "paging"`
+	Loans  []Loan     `json: "loans"`
 }
 
 type UnpagedLoansResponse struct {
-  Loans []Loan `json: "loans"`
+	Loans []Loan `json: "loans"`
 }
 
 func (c *Client) GetLoansByID(loanIDs ...int) ([]Loan, error) {
-  // not sure whether requesting 50 loan IDs will return paged results
+	// not sure whether requesting 50 loan IDs will return paged results
 
-  var baseUrl = "/v1/loans/"
-  var url string
-  var loans []Loan
-  if len(loanIDs) == 0 {
-    return loans, errors.New("No Loan IDs passed")
-  }
-  for i, v := range loanIDs {
-    if i == 0 {
-      char := strconv.Itoa(v)
-      url += char
-    } else {
-      char := strconv.Itoa(v)
-      url += "," + char
-    }
-  }
-  
-  var lr UnpagedLoansResponse
-  err := c.do("GET", baseUrl + url, nil, nil, &lr)
-  if err != nil {
-    return nil, err
-  }
+	var baseUrl = "/v1/loans/"
+	var url string
+	var loans []Loan
+	if len(loanIDs) == 0 {
+		return loans, errors.New("No Loan IDs passed")
+	}
+	for i, v := range loanIDs {
+		if i == 0 {
+			char := strconv.Itoa(v)
+			url += char
+		} else {
+			char := strconv.Itoa(v)
+			url += "," + char
+		}
+	}
 
-  return lr.Loans, nil
+	var lr UnpagedLoansResponse
+	err := c.do("GET", baseUrl+url, nil, nil, &lr)
+	if err != nil {
+		return nil, err
+	}
+
+	return lr.Loans, nil
 }
 
 func (c *Client) GetLoanJournalEntries(loanID int) {
-  // not sure this is even implemented... they don't seem
-  // to publish a schema for it either
-  //baseUrl := fmt.Sprintf("/v1/loans/%d/journal_entries", loanID)
+	// not sure this is even implemented... they don't seem
+	// to publish a schema for it either
+	//baseUrl := fmt.Sprintf("/v1/loans/%d/journal_entries", loanID)
 }
 
 type PagedLendersResponse struct {
-  Paging PagingData `json: "paging"`
-  Lenders []Lender `json: "lenders"`
+	Paging  PagingData `json: "paging"`
+	Lenders []Lender   `json: "lenders"`
 }
 
 func (c *Client) GetLoanLenders(loanID int) ([]Lender, error) {
 
-  baseUrl := fmt.Sprintf("/v1/loans/%d/lenders", loanID)
-  var lr PagedLendersResponse
+	baseUrl := fmt.Sprintf("/v1/loans/%d/lenders", loanID)
+	var lr PagedLendersResponse
 
-  err := c.do("GET", baseUrl, nil, nil, &lr)
-  if err != nil {
-    return nil, err
-  }
-  // FIXME: need to return ALL pages
-  return lr.Lenders, nil
+	err := c.do("GET", baseUrl, nil, nil, &lr)
+	if err != nil {
+		return nil, err
+	}
+	// FIXME: need to return ALL pages
+	return lr.Lenders, nil
 }
 
 // type PagedLoanRepaymentsResponse struct {
@@ -205,13 +207,18 @@ func (c *Client) GetLoanLenders(loanID int) ([]Lender, error) {
 // }
 
 func (c *Client) GetSimilarLoans(loanID int) ([]Loan, error) {
-  baseUrl := fmt.Sprintf("/v1/loans/%d/similar", loanID)
-  var lr UnpagedLoansResponse
+	baseUrl := fmt.Sprintf("/v1/loans/%d/similar", loanID)
+	var lr UnpagedLoansResponse
 
-  err := c.do("GET", baseUrl, nil, nil, &lr)
-  if err != nil {
-    return nil, err
-  }
+	err := c.do("GET", baseUrl, nil, nil, &lr)
+	if err != nil {
+		return nil, err
+	}
 
-  return lr.Loans, nil
+	return lr.Loans, nil
+}
+
+// top-level keys: [u'paging', u'teams']
+// 'teams'[0] keys: [u'category', u'membership_type', u'name', u'member_count', u'image', u'loaned_amount', u'whereabouts', u'loan_because', u'team_since', u'loan_count', u'shortname', u'id', u'website_url', u'description']
+func (c *Client) GetLoanTeams(loanID int) ([]Team, error) {
 }
